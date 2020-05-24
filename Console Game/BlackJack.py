94cd2f9317547e998048d9726875ffead41a269c
RiccardoBefi <65823698+RiccardoBefi@users.noreply.github.com>
2020-05-24 21:33:26 +0200

    Update BlackJack.py

--- Console Game/BlackJack.py
+++ Console Game/BlackJack.py
@@ -7,8 +7,6 @@ def check_input_b(players = 1, decks = 1, decision = 1):
         raise ValueError("Number of players can't be lower than 1")
     if decks < 1:
         raise ValueError("Number of decks must be at least 1")
-    if decision not in [0,1]:
-        raise ValueError("Command unknown, you must enter 0 or 1")
 
 inp = int(input("What is the number of players you desire?\n"))
 check_input_b(inp)
